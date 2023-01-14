077f26f9365d1efd1d5aeafd1d2416b49a8f53d6
jlappen <jlappen@petsmart.com>
2023-01-14 14:23:37 -0700

    feat: eth-gobbler accessibility

---
+++ src/App.js
@@ -0,0 +1,342 @@
+import * as React from "react";
+import "./style.css";
+
+export function App() {
+  return (
+    <React.Fragment>
+      <article className="wrapper">
+        <h2 className="fluid-type">Welcome to the Universe of ETH Gobblers</h2>
+        <div className="extra-black">
+          Gamified Blockchain patronage for a new digital era.
+        </div>
+
+        <section className="stack" aria-labelledby="elastic-supply">
+          <h3 className="fluid-type" id="elastic-supply">
+            What is elastic supply?
+          </h3>
+          <p>
+            Elastic supply refers to the combination of the novel burning
+            mechanism [deflationary] and the ability to split your GOOEYs into 2
+            separate GOOEYs [inflationary].
+          </p>
+
+          <p>
+            The initial GOOEY supply of the ETH Gobblers collection is 2000, but
+            this number increases as GOOEYs undergo mitosis and decreases when
+            GOOEYs get buried. GOOEY owners may perform various activities to
+            keep GOOEYs alive, or let them die and pass on to the next realm.
+          </p>
+        </section>
+
+        <section className="stack" aria-labelledby="what-is-a-gooey">
+          <h3 className="fluid-type" id="what-is-a-gooey">
+            What is a GOOEY?
+          </h3>
+          <p>
+            A GOOEY is your own living, breathing, dynamic creature NFT that
+            lives in a super cute Tamagotchi-style web 3 application. GOOEYs
+            need your love and support to thrive, divide into more GOOEYs and
+            drop gifts along the way.
+          </p>
+
+          <p>
+            Each GOOEY has it's own base traits, disposition, taxonomy and age.
+            GOOEYs can also be equipped with new unique add-ons, claimable to
+            those who keep their GOOEYs healthy and happy.
+          </p>
+
+          <p>
+            Each GOOEY and base traits were hand designed with love by Omakasea
+            Art Director Dudette. [
+            <a href="https://twitter.com/DatArtPerson" target="_blank">
+              https://twitter.com/DatArtPerson
+            </a>
+            ]
+          </p>
+        </section>
+
+        <section className="stack" aria-labelledby="care">
+          <h3 className="fluid-type" id="care">
+            Caring for your GOOEY
+          </h3>
+          <p>
+            Every GOOEY has a health bar which loses health points [HP]
+            gradually over time. If your GOOEY's health bar runs out, your GOOEY
+            dies and may get buried forever. This means if people don't care for
+            their GOOEY, the supply will decrease. Keep in mind, as HP
+            decreases, it becomes more difficult to care for your GOOEY.
+          </p>
+
+          <h3 className="fluid-type">
+            What happens if your GOOEY's health bar goes to 0?
+          </h3>
+          <p>
+            When your GOOEY's health falls to 0, it becomes at risk of getting
+            buried. Anyone can call the "bury" function on a GOOEY that has lost
+            all of it's HP. Calling this function sends the GOOEY to the burn
+            address, decreasing it from the supply forever. You can revive your
+            GOOEY by feeding it before it gets buried to restore HP.
+          </p>
+        </section>
+
+        <h3 className="fluid-type">
+          What happens when your GOOEY health bar is full?
+        </h3>
+        <p>
+          Your GOOEY is healthy. It will reward you with special prizes over
+          time.
+        </p>
+
+        <h3 className="fluid-type">
+          How much ETH does it take to split your GOOEY into 2?
+        </h3>
+        <p>
+          The amount of ETH it takes to split your GOOEY is proportional to the
+          floor price and depends on the elastic supply of the collection. As
+          the supply grows, the ETH to mitosis increases to slow inflation. As
+          more GOOEYs get buried, the ETH to mitosis decreases to boost
+          inflation.
+        </p>
+
+        <p>
+          Certain drops produced by healthy GOOEYs that when equipped, decrease
+          the amount of the ETH needed to undergo mitosis. For example, one of
+          the more rare drops gives a 50% discount off the base cumulative cost.
+        </p>
+
+        <h3 className="fluid-type">
+          What can I do with the GOOEY my GOOEY buds?
+        </h3>
+        <p>
+          Anything you want! Your new GOOEY is an entirely separate ERC-21 token
+          which can then be sold on secondary marketplaces, given away to a
+          friend/family/community member, or kept safe in your wallet.
+        </p>
+
+        <h3 className="fluid-type">How do you gain health points [HP]?</h3>
+        <p>
+          You can raise your GOOEY's health by feeding ETH to your GOOEY,
+          grooming it, or putting it to sleep.
+        </p>
+        <p>
+          Feed: Feed your GOOEY 1x, 5x or FEAST pert TX. FEAST calculates the
+          amount of ETH remaining that is needed to undergo mitosis and feeds
+          your GOOEY that amount.
+        </p>
+        <p>
+          Groom: Groom your GOOEY once per TX. Grooming increases your HP by a
+          significantly larger amount than feeding.
+        </p>
+        <p>
+          Sleeping: Put your GOOEY to sleep once per TX. Sleeping restores 100%
+          of HP but comes with side effects. Your GOOEY remains asleep for a
+          random time (no longer than one week). While asleep, HP decreases at a
+          slower than usual rate and feeding nor grooming can be done.
+        </p>
+
+        <h3 className="fluid-type">
+          What other benefits are there to keeping my GOOEY alive?
+        </h3>
+        <p>
+          Traits: An endlessly growing collection of variable rarity, claimable,
+          unlockable traits that you can equip to your GOOEY with various
+          utilities.
+        </p>
+        <p>
+          Alpha: Secret in-app alpha drops [think important points from Twitter
+          Spaces, insider secrets that are too based to talk about openly,
+          access to pre-recorded content, etc.]
+        </p>
+        <p>One-of-One Art: unlock special one-of-one artworks.</p>
+
+        <h3 className="fluid-type">
+          Where does the ETH you feed to your GOOEY go?
+        </h3>
+        <p>
+          The ETH you feed to your GOOEY goes back to the Omakasea development
+          team. This ETH will be used for: putting food in our team's mouths,
+          supporting our families, and funding development so we can continue
+          making dop shit for the ecosystem [such as mglth.tv, omakasea.com, and
+          etc.]. We hope that the ETH Gobblers model can be used by other
+          teams/artists/developers to boost their operations.
+        </p>
+
+        <h3 className="fluid-type">
+          How can the ETH Gobblers model support artists and creators through a
+          new form of gamified patronage?
+        </h3>
+        <p>
+          The ETH Gobblers model is a gamification that incentivizes holders
+          [via claimable drops, community access, or whatever the creator
+          decides] to deposit ETH into a contract over time. We believe that
+          this is an ideal on-chain solution as an alternative to softwares such
+          as Patreon, Kofi, etc. to help independent project teams get ahead.
+        </p>
+      </article>
+
+      <article className="wrapper">
+        <h2 className="fluid-type">How to Play</h2>
+
+        <h3 className="fluid-type">HP Breakdown</h3>
+        <p>
+          Your Gooey will lose between 6 and 10 health daily, this variable is
+          randomized. Pricing for performing each action is subject to change,
+          and is gnerally based on the Average Gooey floor price over the past
+          24 hours.
+        </p>
+
+        <table className="play-table">
+          <thead>
+            <tr>
+              <th className="col-h-1">Feeding</th>
+              <th className="col-h-2">Grooming</th>
+              <th>Sleeping</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="col-b-1">
+                .001 ETH to Feed x1. 1 Feed increases health by 5 HP. Feasting
+                triggers mitosis, the mitosis threshold is 20% below time
+                weighted average floor price as determined by marketplace and
+                aggregator APIs.
+              </td>
+              <td className="col-b-2">
+                .01 ETH to Groom increases health by 55 HP.
+              </td>
+              <td>
+                .03 ETH to put your Gooey into a state of hibernation lasting
+                anywhere from 4-7 days, this time variable is randomized. While
+                sleeping, your Gooey loses half as much HP per day (3-5 HP).
+                Sleeping increases health by 100 HP.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h3 className="fluid-type">The Gooey Gobbler</h3>
+        <p>
+          At all times one random Gooey is the Gooey Gobbler. The Gooey Gobbler
+          can pay a fee to the contract to gobble anyone else's Gooey. When that
+          Gooey is gobbled, the gobbler receives the Gooey that they gobbled,
+          and the Gobbler role is assigned to a new random Gooey. Don't worry
+          though, as it is in life, if misfortune befalls you and your Gooey is
+          gobbled, you may find that good fortune is just around the corner.
+          This functionality has yet to go live, but we will give more specific
+          information about it when it does.
+        </p>
+      </article>
+
+      <article className="wrapper">
+        <h2 className="fluid-type">GobDrops</h2>
+
+        <p>
+          If you have cumulatively fed your Gooey .05 ETH, you will be eligible
+          for trait drops when they occur. Please note that this price is
+          subject to change depending on the particular drop, as there are
+          different types of drops.
+        </p>
+
+        <p>
+          Besides just making your Gooey look cooler and more unique, traits can
+          provide extra benefits when it comes to taking care of your Gooey.
+          Some unlockable traits possess hidden magical powers that will make it
+          easier to care for your Gooey, such as slowing down the rate of HP
+          loss, feeding boosts, and other yet to be revealed functionality.
+        </p>
+
+        <p>
+          We will be releasing a more definite timeline of upcoming GobDrops in
+          the coming week, with the first trait drop for all holders coming in
+          the first week of January, 2023. The traits included in these drops
+          are their own ERC-721 NFTs that can be traded openly, and are not
+          influenced at all if you sell your Gooey, your Gooey dies, or your
+          Gooey gets gobbled by the Gooey Gobbler.
+        </p>
+
+        <div className="extra-black">
+          Types of traits: background, weather, cushion, food, wings,
+          accessories, sidekicks, inflight.
+        </div>
+
+        <h3 className="fluid-type">
+          There are 3 collection supply possibilities for GobDrops
+        </h3>
+        <ul>
+          <li className="extra-black">
+            Generative trait collection (animated or still image)
+          </li>
+          <li className="extra-black">
+            1/1s editions: each 1/1 can have 0-1000 editions
+          </li>
+          <li className="extra-black">1/1s: only one available</li>
+        </ul>
+
+        <h3 className="fluid-type">
+          There are 3 creation/distribution models for GobDrops
+        </h3>
+        <ul>
+          <li className="extra-black">
+            In-house drops: Crafted solely by Dudette {`&`} core team
+          </li>
+          <li>
+            <span className="extra-black">Artist-in-residence program: </span>
+            <p className="display-initial">
+              Omakasea is holding an open call for visual artists (illustrators,
+              animators) to work with us for a period of time (ideally 2-3
+              months) to create special 1/1s editions and equippable traits for
+              the gooeys. The artist in residence will receive a stipend and
+              name credit for their work.
+            </p>
+          </li>
+          <li>
+            <span className="extra-black">Community Collab: </span>
+            <p className="display-initial">
+              We will reach out to other Web3 communities that we like (ex:
+              Doodles, Milady Maker, etc.) to help us curate and create
+              equippable traits, special 1/1s editions, and super rare
+              background 1/1s, that are claimable by Gooey holders who also hold
+              the collaborating collections in their wallet as well as other
+              wallets that they link via signature to the ETH Gobblers dApp.
+            </p>
+          </li>
+        </ul>
+
+        <div className="footer">
+          <div>
+            <span className="extra-black">Artist Residencies</span>
+            <p>
+              If you are interested in applying for our Artist Residency
+              program, you can do so via this form:{" "}
+              <a
+                href="https://docs.google.com/document/d/1yfcEX3wMrBF5jlYnvYtB64lpdajEDWWlyzVqv9a7lxl/edit"
+                target="_blank"
+              >
+                https://docs.google.com/document/d/1yfcEX3wMrBF5jlYnvYtB64lpdajEDWWlyzVqv9a7lxl/edit
+              </a>
+            </p>
+          </div>
+
+          <div className="mt-2">
+            <span className="extra-black">The Gooey Dungeons</span>
+            <p>Dungeon challenges will begin at the end of Q1.</p>
+          </div>
+
+          <div className="mt-2">
+            <span className="extra-black">Gooey Disposition</span>
+            <p>
+              Each Gooey was minted via an invite link which indicated whether
+              the minter was on the Naughty list or the Nice list. This impacts
+              the disposition of your Gooey. Naughty Gooeys have 2 face types,
+              and Nice Gooeys also have 2 face types. When you perform Mitosis
+              on a Gooey, the disposotion follows from the parent. As of now,
+              there are no plans for functionality based on the disposition of
+              Gooeys, it's just a fun trait that speaks to the character of your
+              Gooey.
+            </p>
+          </div>
+        </div>
+      </article>
+    </React.Fragment>
+  );
+}
